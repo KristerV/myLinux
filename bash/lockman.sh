a24cfc902b25cb1b4833ea9ff7456e862a0316ae
Krister Viirsaar <git@krister.ee>
2020-04-11 13:59:01 +0300

    2020-04-11 backup

--- bash/lockman.sh
+++ bash/lockman.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 swayidle \
-    timeout 10 'swaymsg "output * dpms off"' \
+    timeout 30 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
 # kill last background task
